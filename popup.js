b5105d08aee4403ec97b7b9644b27d5f7c1c4fe9
Drake Bennion <drakebennion@gmail.com>
2016-12-31 13:14:49 -0700

    add windows button and a windows shortcut, slight refactor of buttonclick method

--- popup.js
+++ popup.js
@@ -1,30 +1,34 @@
 var chromeButton = document.getElementById('chrome-button');
 var sublimeButton = document.getElementById('sublime-button');
+var windowsButton = document.getElementById('windows-button');
 
 var shortcutDivs = document.querySelectorAll('.shortcut-div');
 var shortcutButtons = document.querySelectorAll('.button');
 
-chromeButton.onclick = buttonClick.bind(this, 'chrome');
-sublimeButton.onclick = buttonClick.bind(this, 'sublime');
+chromeButton.onclick = buttonClick.bind(this, 'chrome', chromeButton);
+sublimeButton.onclick = buttonClick.bind(this, 'sublime', sublimeButton);
+windowsButton.onclick = buttonClick.bind(this, 'windows', windowsButton);
 
-//gross. this is cheating, isn't it?
 document.onkeydown = function (event) {
 	var alt = event.altKey;
 	if (alt) {
 		switch (event.key) {
 			case 'c':
-				buttonClick.call(this, 'chrome', { currentTarget: chromeButton });
+				buttonClick.call(this, 'chrome', chromeButton);
 				break;
 			case 'u':
-				buttonClick.call(this, 'sublime', { currentTarget: sublimeButton });
+				buttonClick.call(this, 'sublime', sublimeButton);
+				break;
+			case 'w':
+				buttonClick.call(this, 'windows', windowsButton);
 				break;
 		}
 	}
 };
 
-function buttonClick(name, event) {
+function buttonClick(name, target) {
 	shortcutButtons.forEach(function (button) {
-		if (button !== event.currentTarget) {
+		if (button !== target) {
 			button.classList.remove('active');
 		}
 		else {
